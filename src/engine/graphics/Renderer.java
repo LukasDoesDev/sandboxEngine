0c26faf61e3507982e1aad0583c52761021d9906
Lukas562 <axel.karjalainen@gmail.com>
2020-03-27 15:02:17 +0200

    Binding shader to mesh when rendering mesh...

--- src/engine/graphics/Renderer.java
+++ src/engine/graphics/Renderer.java
@@ -33,7 +33,9 @@ public class Renderer {
         GL30.glEnableVertexAttribArray(0);
         GL30.glEnableVertexAttribArray(1);
         GL15.glBindBuffer(GL15.GL_ELEMENT_ARRAY_BUFFER, mesh.getIBO());
+        shader.bind();
         GL11.glDrawElements(GL11.GL_TRIANGLES, mesh.getIndices().length, GL11.GL_UNSIGNED_INT, 0);
+        shader.unbind();
         GL15.glBindBuffer(GL15.GL_ELEMENT_ARRAY_BUFFER, 0);
         GL30.glDisableVertexAttribArray(0);
         GL30.glDisableVertexAttribArray(1);
